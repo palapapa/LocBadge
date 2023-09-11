319800521c14762b88e631b13b2ab64db5f6a273
palapapa <palapapa93@gmail.com>
2023-09-11 23:10:18 +0800

    Added the Required attribute to LocModel members
    Used the ModelBinder constructor that had a single parameter instead of
    the parameterless one.

--- Models/LocModel.cs
+++ Models/LocModel.cs
@@ -2,15 +2,19 @@ using DotNet.Globbing;
 using LocBadge.ModelBinders;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LocBadge.Models;
 
-[ModelBinder(BinderType = typeof(LocModelBinder))]
+[ModelBinder(typeof(LocModelBinder))]
 public record LocModel
 {
+    [Required]
     public required IList<SearchPath> Paths { get; init; }
 
+    [Required]
     public required IList<string> Extensions { get; init; }
 
+    [Required]
     public required bool AreExtensionsExcluded { get; init; }
 }
